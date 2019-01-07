1b6773ded3c5abb90109db7e0fc6e8371a03042b
xubylele <diazxavier27@gmail.com>
2019-01-07 18:05:33 -0300

    Pagina inicio desechable

--- asistencia/resources/views/layouts/headers.blade.php
+++ asistencia/resources/views/layouts/headers.blade.php
@@ -17,8 +17,32 @@
     <!-- Sweet Alerts 2-->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.28.4/dist/sweetalert2.all.min.js"></script>
 
+
+    <!--Data table-->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/css/bootstrap-select.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/js/bootstrap-select.min.js"></script>
+
+    <!--Rut Formater-->
+    <script src={{asset("js/jquery.rut.js")}}></script>
+
+    <script src="/js/mensajes.js"></script>
+    <script src="/js/notify.js"></script>
+    
+    <link rel="stylesheet" href="/css/navbar.css">
   </head>
   <body>
     @yield('body')
--- asistencia/resources/views/welcome.blade.php
+++ asistencia/resources/views/welcome.blade.php
@@ -1,5 +1,40 @@
 @extends('layouts.headers')
 
 @section('body')
-  <h1>hola</h1>
+  <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+    <div class="container">
+      <a class="navbar-brand" href="{!! route('index') !!}">
+        <h4>{{ config('app.name', 'Laravel') }}</h4>
+      </a>
+      <ul class="navbar-nav ml-auto">
+        @guest
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">{{ __('Ingresar al perfil de administrador') }}</a>
+          </li>
+        @endguest
+      </ul>
+    </div>
+  </nav>
+  <div class="jumbotron">
+    <h1 class="display-4">Bienvenido</h1>
+    <p class="lead">Si desea marcar asistencia ingrese su rut</p>
+    <div class="row">
+      <div class="col-6 col-md-4">
+        <form>
+          <div class="form-group">
+            <input class="form-control" id="rut" type="text" name="rut" placeholder="Ingrese su rut">
+          </div>
+          <div class="form-group">
+            <button class="btn btn-success" type="button">Marcar Asistencia</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $("input#rut").rut({formatOn: 'keyup', ignoreControlKeys: false});
+    });
+
+  </script>
 @endsection
