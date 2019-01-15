2117a054aa39b70e06d4ab06d32133b57b437c08
Francisco Riquelme Cavagnola <francisco.riquelme@outlook.com>
2019-01-15 12:52:18 -0300

    Actualización del listado de obras

--- varmetal2019/varmetal2019/resources/views/admin/administracion_obras.blade.php
+++ varmetal2019/varmetal2019/resources/views/admin/administracion_obras.blade.php
@@ -5,13 +5,14 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Productos</div>
+                <div class="card-header">Listado de Productos</div>
                     <div class="card=body container mt-3">
                         @if(($obras != NULL) && (count($obras) > 0))
                         <table id="tablaAdministracion" style="width:100%" align="center">
                             <thead>
                                 <tr>
                                     <th>Nombre</th>
+                                    <th>Proyecto</th>
                                     <th>Opciones</th>
                                 </tr>
                             </thead>
@@ -19,6 +20,7 @@
                                 @foreach($obras as $key => $obra)
                                 <tr id="id_obra{{ $obra->idObra }}">
                                     <td scope="col">{{ $obra->nombre }}</td>
+                                    <td scope="col">{{ $obra->proyecto }}</td>
                                     <td><a class="btn btn-outline-success my-2 my-sm-0" href="{{url('obraControl', [$obra->idObra])}}" role="button" style="cursor: pointer;">Ver Detalles</a></td>
                                 </tr>
                                 @endforeach
