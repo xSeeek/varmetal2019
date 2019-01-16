97866f32d7719e80aae1f3637c8c17b3a823a070
xubylele <diazxavier27@gmail.com>
2019-01-16 16:48:02 -0300

    Detalles asistencia con imagen

--- asistencia/resources/views/asistencia/detallesAsistencia.blade.php
+++ asistencia/resources/views/asistencia/detallesAsistencia.blade.php
@@ -2,13 +2,13 @@
 
 @section('main')
   <div class="container mt-2">
-    <div class="row">
-      <div class="col-sm-5">
-        <div class="card">
-          <div class="card-header">
-            <h3 class="card-tittle">Detalles de la asistencia de {{$trabajador->nombre}}</h3>
-          </div>
-          <div class="card-body">
+    <div class="card">
+      <div class="card-header">
+        <h3 class="card-tittle">Detalles de la asistencia de {{$trabajador->nombre}}</h3>
+      </div>
+      <div class="card-body">
+        <div class="row">
+          <div class="col">
             <div class="form-group">
               <label for="rut">Rut:</label>
               <input type="text" name="rut" value="{{$trabajador->rut}}" class="form-control" disabled>
@@ -22,12 +22,11 @@
               <input type="text" name="fecha" value="{{$asistencia->created_at}}" class="form-control" disabled>
             </div>
           </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <div class="card-header">
-            
+          <div class="col">
+            <figure class="figure">
+              <img src="{{ Storage::disk('asistencia')->url($trabajador->rut.'/'.$asistencia->image) }}" class="figure-img img-fluid rounded">
+              <figcaption class="figure-caption text-right">Imagen tomada el día en que se marco la asistencia.</figcaption>
+            </figure>
           </div>
         </div>
       </div>
