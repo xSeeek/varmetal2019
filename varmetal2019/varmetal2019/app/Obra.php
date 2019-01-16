86d7973e746a82a9110a421e7132f09c2bf02bdb
Patricio Torres <Patricio.igtr@gmail.com>
2019-01-16 15:11:14 -0300

    wea mala hermano

--- varmetal2019/varmetal2019/app/Obra.php
+++ varmetal2019/varmetal2019/app/Obra.php
@@ -9,16 +9,16 @@ class Obra extends Model
     public $primaryKey = 'idObra';
     protected $table = 'obra';
 
+    public function supervisorWithAtributes()
+    {
+        return $this->belongsToMany('Varmetal\Trabajador', 'obra_supervisor', 'obras_id_obra', 'trabajador_id_trabajador', )->withPivot('tiempoPerdido', 'tiempoSetUp');
+    }
     public function producto()
     {
         return $this->hasMany('Varmetal\Producto', 'obras_id_obra');
     }
-    public function supervisorWithAtributes()
-    {
-        return $this->belongsToMany('Varmetal\Trabajador', 'obra_supervisor', 'trabajador_id_trabajador', 'obras_id_obra')->withPivot('tiempoPerdido', 'tiempoSetUp');
-    }
     public function supervisor()
     {
-        return $this->belongsToMany('Varmetal\Trabajador', 'obra_supervisor', 'trabajador_id_trabajador', 'obras_id_obra');
+        return $this->belongsToMany('Varmetal\Trabajador', 'obra_supervisor','obras_id_obra', 'trabajador_id_trabajador', );
     }
 }
