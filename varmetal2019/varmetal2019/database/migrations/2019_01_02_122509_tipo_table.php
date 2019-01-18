29393b77e4c12a8471b6a9bf7f2b88f7ccbed52a
Francisco Riquelme Cavagnola <francisco.riquelme@outlook.com>
2019-01-18 16:56:47 -0300

    Update

--- varmetal2019/varmetal2019/database/migrations/2019_01_02_122509_tipo_table.php
+++ varmetal2019/varmetal2019/database/migrations/2019_01_02_122509_tipo_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class TipoPiezaTable extends Migration
+class TipoTable extends Migration
 {
     /**
      * Run the migrations.
