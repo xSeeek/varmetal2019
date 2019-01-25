e69d741430919a844d30e82e7fb82f7d69c35f82
xubylele <diazxavier27@gmail.com>
2019-01-25 15:54:13 -0300

    blablabla

--- asistencia/app/Http/Controllers/AsistenciaController.php
+++ asistencia/app/Http/Controllers/AsistenciaController.php
@@ -41,7 +41,8 @@ class AsistenciaController extends Controller
 
   public function registrarAsistencia(MarcarAsistencia $request)
   {
-    var_dump($request->file('file'));/*
+    var_dump($request->file('file'));
+
     if($request->hasFile('file'))
     {
       $trabajador = Trabajador::where('rut', $request->rut)->first();
@@ -80,7 +81,7 @@ class AsistenciaController extends Controller
         ->with('error', 'El trabajador ingresado no posee una obra asignada');
     }else {
       return 'esta malloc';
-    }*/
+    }
   }
 
   public function detallesAsistencia($rut, $id)
