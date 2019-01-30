abf89d4917c2469eb88c1582b9cb2a03958d5c86
xubylele <diazxavier27@gmail.com>
2019-01-30 11:05:15 -0300

    limitando subida de imagenes

--- asistencia/resources/views/home.blade.php
+++ asistencia/resources/views/home.blade.php
@@ -20,7 +20,7 @@
             </div>
 
             <div class="form-group">
-              {{Form::file('file', array('class' => 'form-control-file', 'id' => 'img_select', 'required'))}}
+              {{Form::file('file', array('class' => 'form-control-file', 'id' => 'img_select', 'required', 'capture' => 'camera'))}}
             </div>
 
             <div class="form-group">
