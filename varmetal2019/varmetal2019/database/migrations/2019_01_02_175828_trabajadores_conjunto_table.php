afe66a297cf91a8cc2183216a77ec23cb61f56eb
Francisco Riquelme Cavagnola <francisco.riquelme@outlook.com>
2019-01-30 11:47:32 -0300

    Update migrate

--- varmetal2019/varmetal2019/database/migrations/2019_01_02_175828_trabajadores_conjunto_table.php
+++ varmetal2019/varmetal2019/database/migrations/2019_01_02_175828_trabajadores_conjunto_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class TrabajadoresProductoTable extends Migration
+class TrabajadoresConjuntoTable extends Migration
 {
     /**
      * Run the migrations.
@@ -35,6 +35,6 @@ class TrabajadoresProductoTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('trabajadores_producto');
+        Schema::dropIfExists('trabajadores_conjunto');
     }
 }
