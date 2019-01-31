ccc5ce921307ca3a70b1f5ef311f858f27f40a48
Francisco Riquelme Cavagnola <francisco.riquelme@outlook.com>
2019-01-31 16:27:50 -0300

    Importar OT sin header listo para su uso

--- varmetal2019/varmetal2019/app/Http/Controllers/ImportController.php
+++ varmetal2019/varmetal2019/app/Http/Controllers/ImportController.php
@@ -7,6 +7,7 @@ use Varmetal\Http\Requests\CsvImportRequest;
 use Carbon\Carbon;
 use Varmetal\CSVData;
 use Varmetal\Obra;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ImportController extends Controller
 {
@@ -22,17 +23,33 @@ class ImportController extends Controller
     public function parseImport(CsvImportRequest $request)
     {
         $path = $request->file('csv_file')->getRealPath();
-        $data = array_map('str_getcsv', file($path));
 
-        $csv_data_file = CSVData::create([
-            'csv_filename' => $request->file('csv_file')->getClientOriginalName(),
-            'csv_header' => $request->has('header'),
-            'csv_data' => json_encode($data),
-            'csv_table' => $request->table,
-        ]);
+        if ($request->has('header'))
+            $data = Excel::load($path, function($reader) {})->get()->toArray();
+        else
+            $data = array_map('str_getcsv', file($path));
 
-        $csv_data = array_slice($data, 0, count($data));
-        return view('admin.import.import_fields', compact('csv_data', 'csv_data_file'));
+        if (count($data) > 0)
+        {
+            if ($request->has('header'))
+            {
+                $csv_header_fields = [];
+                foreach ($data[0] as $key => $value)
+                    $csv_header_fields[] = $key;
+            }
+            $csv_data = array_slice($data, 0, count($data));
+
+            $csv_data_file = CSVData::create([
+                'csv_filename' => $request->file('csv_file')->getClientOriginalName(),
+                'csv_header' => $request->has('header'),
+                'csv_data' => json_encode($data),
+                'csv_table' => $request->table,
+            ]);
+        }
+        else
+            return redirect()->back();
+
+        return view('admin.import.import_fields', compact('csv_header_fields', 'csv_data', 'csv_data_file'));
     }
     public function processImport(Request $request)
     {
@@ -47,17 +64,19 @@ class ImportController extends Controller
 
     public function importObras($data, $request)
     {
+        $data_csv = CsvData::find($request->csv_data_file_id);
         foreach ($data as $row)
         {
             $obra = new Obra();
             foreach (config('app.db_obras') as $index => $field)
             {
-                if((Obra::where('codigo', $request->fields[$index])->first()) != NULL)
-                    break;
-                $obra->$field = $row[$request->fields[$index]];
-                if($field == 'fechaInicio')
-                    $obra->$field = $obra->$field;
+                if ($data_csv->csv_header)
+                    $obra->$field = $row[$request->fields[$field]];
+                else
+                    $obra->$field = $row[$request->fields[$index]];
             }
+            if((Obra::where('codigo', $obra->codigo)->first()) != NULL)
+                continue;
             if($obra->fechaInicio = 'No determinada')
                 $obra->fechaInicio = (new Carbon())->now();
             else
@@ -66,4 +85,25 @@ class ImportController extends Controller
         }
         return;
     }
+
+    private function file_post_contents($url, $data)
+    {
+        $postdata = http_build_query($data);
+
+        $opts = array('http' =>
+            array(
+                'method'  => 'POST',
+                'header'=> "Content-type: application/x-www-form-urlencoded\r\n"
+                . "X-CSRF-TOKEN: " . csrf_token() . "\r\n",
+                'content' => $postdata
+            )
+        );
+
+        $context = stream_context_create($opts);
+        return file_get_contents($url, false, $context);
+    }
+
+    /*$data = array("rut" => "11.111.112-K");
+    var_dump($this->file_post_contents('http://gestion.varmetal.cl/asistencia/obtenerAsistencia', $data));
+    return;*/
 }
