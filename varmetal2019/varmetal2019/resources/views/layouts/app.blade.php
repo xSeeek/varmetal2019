a6aad20a86a6087b8f427a1323e105a0abedeed2
Francisco Riquelme Cavagnola <francisco.riquelme@outlook.com>
2019-02-01 12:13:52 -0300

    Update Ver Pausas para admin

--- varmetal2019/varmetal2019/resources/views/layouts/app.blade.php
+++ varmetal2019/varmetal2019/resources/views/layouts/app.blade.php
@@ -79,14 +79,14 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                      <a class="btn btn-outline-primary btn-md" id="boton" onclick="detenerPausa()" role="button" style="display:none;">Ver Pausas</a>
                         @guest
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                         @else
                             @if(Auth::user()->type == 'Supervisor')
-                              <audio id="alarma"></audio> <!--autoplay loop-->
+                                <a class="btn btn-outline-primary btn-md" id="boton" onclick="detenerPausa()" role="button" style="display:none;">Ver Pausas</a>
+                                <audio id="alarma"></audio> <!--autoplay loop-->
                             @endif
                             <li class="nav-item dropdown">
                               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
