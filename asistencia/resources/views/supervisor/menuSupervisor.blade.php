7f0d2dcb590c3623b81fbae68695686f7d0e52e0
xubylele <diazxavier27@gmail.com>
2019-02-27 09:23:02 -0300

    corrigiendo error obra == null

--- asistencia/resources/views/supervisor/menuSupervisor.blade.php
+++ asistencia/resources/views/supervisor/menuSupervisor.blade.php
@@ -2,57 +2,65 @@
 
 @section('main')
   <div class="container mt-2 mb-3">
-    <div class="card">
-      <div class="card-header">
-        <h3 class="card-tittle">Administrar Obras, Obra: {{$obra->nombre}}</h3>
-      </div>
-      <div class="card-body">
-        <div class="form-group">
-          <label for="nombre">Nombre de la obra</label>
-          <input type="text" id='nombre' class="form-control" value="{{$obra->nombre}}" disabled>
+    @if($obra == null)
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-tittle">No tiene una obra asignada</h3>
         </div>
       </div>
-    </div>
-    <br>
-    <div class="card">
-      <div class="card-header">
-        <h3 class="card-tittle">Personal de la obra</h3>
+    @else
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-tittle">Administrar Obras, Obra: {{$obra->nombre}}</h3>
+        </div>
+        <div class="card-body">
+          <div class="form-group">
+            <label for="nombre">Nombre de la obra</label>
+            <input type="text" id='nombre' class="form-control" value="{{$obra->nombre}}" disabled>
+          </div>
+        </div>
       </div>
-      <div class="card-body">
-        @if (count($obra->trabajadores) == 1)
-          <h2>No se registran trabajadores en esta obra</h2>
-        @else
-          <table id="tabla_trabajadores" class="table display" style="width:100%">
-            <thead class="thead-dark">
-              <tr>
-                <th class="text-center">Rut</th>
-                <th class="text-center">Nombre</th>
-                <th class="text-center" data-toggle="tooltip" title="Configuraciones"><i class="fas fa-cogs"></i></th>
-              </tr>
-            </thead>
-            <tbody>
-              @foreach ($obra->trabajadores as $trabajador)
-              <tr>
-                <td class="text-center">{{$trabajador->rut}}</td>
-                <td class="text-center">{{$trabajador->nombre}}
-                  @if ($trabajador->user != null)
-                    @if($trabajador->user->isSupervisor())
-                      (Supervisor)
+      <br>
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-tittle">Personal de la obra</h3>
+        </div>
+        <div class="card-body">
+          @if (count($obra->trabajadores) == 1)
+            <h2>No se registran trabajadores en esta obra</h2>
+          @else
+            <table id="tabla_trabajadores" class="table display" style="width:100%">
+              <thead class="thead-dark">
+                <tr>
+                  <th class="text-center">Rut</th>
+                  <th class="text-center">Nombre</th>
+                  <th class="text-center" data-toggle="tooltip" title="Configuraciones"><i class="fas fa-cogs"></i></th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($obra->trabajadores as $trabajador)
+                <tr>
+                  <td class="text-center">{{$trabajador->rut}}</td>
+                  <td class="text-center">{{$trabajador->nombre}}
+                    @if ($trabajador->user != null)
+                      @if($trabajador->user->isSupervisor())
+                        (Supervisor)
+                      @endif
                     @endif
-                  @endif
-                </td>
-                <td class="text-center">
-                  <div class="btn-group" role="group">
-                    <a role="button" class="btn btn-primary font-weight-bold text-light" href="{!! route('supervisor.verAsistencia', ['rut'=>$trabajador->rut]) !!}">Ver Asistencia</a>
-                  </div>
-                </td>
-              </tr>
-            @endforeach
-            </tbody>
-          </table>
-        @endif
+                  </td>
+                  <td class="text-center">
+                    <div class="btn-group" role="group">
+                      <a role="button" class="btn btn-primary font-weight-bold text-light" href="{!! route('supervisor.verAsistencia', ['rut'=>$trabajador->rut]) !!}">Ver Asistencia</a>
+                    </div>
+                  </td>
+                </tr>
+              @endforeach
+              </tbody>
+            </table>
+          @endif
+        </div>
       </div>
-    </div>
+    @endif
   </div>
 
   <script type="text/javascript">
